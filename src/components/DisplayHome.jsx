2a06b9f7f0f6e4ad6a4e750c970a5e712d6670d3
psaisuryatejareddy <saitejageneral@gmail.com>
2024-07-09 19:20:02 +0530

    third Commit

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Navbar from './Navbar'
-import { albumsData } from '../assets/assets'
-import Albumitem from './Albumitem'
+import { albumsData, songsData } from '../assets/assets'
+import SongItem from './SongItem'
+import AlbumItem from './AlbumItem'
+
 
 const DisplayHome = () => {
     return (
@@ -10,17 +12,16 @@ const DisplayHome = () => {
             <div className='mb-4'>
                 <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
                 <div className='flex overflow-auto'>
-                    {albumsData.map((item, index) => (<Albumitem key={item} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+                    {albumsData.map((item, index) => (<AlbumItem key={item} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
                 </div>
 
             </div>
             <div className='mb-4'>
-            <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
-            <div className='flex overflow-auto'>
-                
+                <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
+                <div className='flex overflow-auto'>
+                    {songsData.map((item, index) => (<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+                </div>
             </div>
-
-        </div>
         </>
     )
 }
