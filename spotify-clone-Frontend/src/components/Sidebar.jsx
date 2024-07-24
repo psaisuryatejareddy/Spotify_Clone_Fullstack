1ab54666759337a1a46eeb6e2d380936ac4c26fd
psaisuryatejareddy <133512286+psaisuryatejareddy@users.noreply.github.com>
2024-07-25 01:46:42 +0530

    Update Sidebar.jsx

--- spotify-clone-Frontend/src/components/Sidebar.jsx
+++ spotify-clone-Frontend/src/components/Sidebar.jsx
@@ -17,7 +17,7 @@ function Sidebar() {
           <img className='w-6' src={assets.search_icon} alt='' />
           <p className='font-bold'>search</p>
         </div>
-        
+
 
       </div>
       <div className='bg-[#121212] h-[85%] rounded'>
@@ -38,9 +38,16 @@ function Sidebar() {
           <h1>Let's find some podcasts to follow</h1>
           <p className='font-light'>We'll keep you update on new episodes</p>
           <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>Browse podcasts</button></div>
+        <div className='p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4'>
+          <h1>Admin Login</h1>
+          <p className='font-light'>Add/Remove songs and Albums</p>
+          <a href='https://spotify-clone-fullstack-admin-page.vercel.app/add-song'>
+            <button className='px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4'>
+              Admin page
+            </button>
+          </a>
+        </div>
       </div>
-
-
     </div>
   )
 }
